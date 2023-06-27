acca6849d6074f2b4d5a9b7faca2788c0544c1e1
AlexeyIvanov90 <my.name.alexey.ivanov@gmail.com>
2023-06-27 11:34:28 +0300

    modif for siam NN

--- main.py
+++ main.py
@@ -1,41 +1,61 @@
 import os
 import pandas as pd
+import random
 
 # iterators over files in respective folders
-data_1 = os.scandir('data/data_1')
-data_2 = os.scandir('data/data_2')
-
-# create a data frame to save locations and labels
-df = pd.DataFrame(columns=['file_name', 'label'])
+data_1 = os.scandir('../../../source/dataRGB/Основное зерно отобранное')
+data_2 = os.scandir('../../../source/dataRGB/Пшеница битые отобранное')
 
 count = 1
+category_1 = list()
+category_2 = list()
 
 for data in data_1:
-    old_name_file = 'data/data_1/' + data.name
-    new_name_file = 'data/category_1/' + str(count) + ".png"
-    os.rename(old_name_file, new_name_file)
-    count += 1
+    category_1.append(data)
 
-count = 1
 
 for data in data_2:
-    old_name_file = 'data/data_2/' + data.name
-    new_name_file = 'data/category_2/' + str(count) + ".png"
-    os.rename(old_name_file, new_name_file)
-    count += 1
+    category_2.append(data)
 
-categories_1 = os.scandir('data/category_1')
-categories_2 = os.scandir('data/category_2')
+random.shuffle(category_1)
+random.shuffle(category_2)
 
-for category in categories_1:
-    loc = 'data/category_1/{}'.format(category.name)
-    df = df.append({'file_name': loc, 'label': 0}, ignore_index=True)
 
-count = 1
+max_size = min(len(category_1), len(category_2))
 
-for category in categories_2:
-    loc = 'data/category_2/{}'.format(category.name)
-    df = df.append({'file_name': loc, 'label': 1}, ignore_index=True)
+# create a data frame to save locations and labels
+df_train = pd.DataFrame(columns=['file_name_1', 'file_name_2', 'label'])
+df_val = pd.DataFrame(columns=['file_name_1', 'file_name_2', 'label'])
+df_test = pd.DataFrame(columns=['file_name_1', 'file_name_2', 'label'])
+
+j = max_size - 1
+
+for i in range(max_size):
+    df_buf = pd.DataFrame(columns=['file_name_1', 'file_name_2', 'label'])
+
+    df_buf = df_buf._append({'file_name_1': category_1[i].path, 'file_name_2': category_2[j].path, 'label': 1}, ignore_index=True)
+    df_buf = df_buf._append({'file_name_1': category_1[j].path, 'file_name_2': category_1[i].path, 'label': 0}, ignore_index=True)
+    df_buf = df_buf._append({'file_name_1': category_1[j].path, 'file_name_2': category_2[i].path, 'label': 1}, ignore_index=True)
+    df_buf = df_buf._append({'file_name_1': category_2[i].path, 'file_name_2': category_2[j].path, 'label': 0}, ignore_index=True)
+
+    if i < (max_size * 0.8)/2:
+        df_train = df_train._append(df_buf, ignore_index=True)
+    else:
+        if i < (max_size * 0.9)/2:
+            df_val = df_val._append(df_buf, ignore_index=True)
+        else:
+            df_test = df_test._append(df_buf, ignore_index=True)
+
+    j -= 1
+    if i >= j:
+        break
+
+#df = df.sample(frac=1).reset_index(drop=True)
+df_train = df_train.sample(frac=1)
+df_val = df_val.sample(frac=1)
+df_test = df_test.sample(frac=1)
 
 # save as csv file
-df.to_csv('file_names.csv', header=None, index=False)
+df_train.to_csv('train.csv', header=None, index=False)
+df_val.to_csv('val.csv', header=None, index=False)
+df_test.to_csv('test.csv', header=None, index=False)
