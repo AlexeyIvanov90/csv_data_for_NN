5056acde8469f8011e488dd3ea5b6b179cb469fb
AlexeyIvanov90 <my.name.alexey@mail.ru>
2023-07-05 21:41:04 +0300

    add make data set and mod alg

--- main.py
+++ main.py
@@ -30,67 +30,73 @@ def max_size(work_folder, data_max_size=0):
         return data_max_size
 
 
-def make_data_class(work_dir_class, data_max_size=0):
+def make_data_category(work_dir_class):
     datas = os.scandir(work_dir_class)
+    category = list()
 
-    file = list()
     for data in datas:
         if is_png(data.name):
-            file.append(data)
+            category.append(data)
 
-    random.shuffle(file)
-    if data_max_size == 0:
-        return file
-    else:
-        return file[0:data_max_size]
+    random.shuffle(category)
+
+    return category
 
 
-def cat_class(dir_class_1, dir_class_2):
-    df_buf = pd.DataFrame(columns=['file_name_1', 'file_name_2', 'label'])
-    label = 1
-    data_max_size = min(len(dir_class_1), len(dir_class_2))
-
-    if len(dir_class_1) == len(dir_class_2):
-        label = 0
-        for i in range(int(data_max_size/2)):
-            df_buf = df_buf._append(
-                {'file_name_1': dir_class_1[i].path, 'file_name_2': dir_class_2[data_max_size - i - 1].path,
-                 'label': label},
-                ignore_index=True)
-            df_buf = df_buf._append(
-                {'file_name_1': dir_class_2[i].path, 'file_name_2': dir_class_1[data_max_size - i - 1].path,
-                 'label': label},
-                ignore_index=True)
-    else:
-        for i in range(int(data_max_size/2)):
-            df_buf = df_buf._append(
-                {'file_name_1': dir_class_1[i].path, 'file_name_2': dir_class_2[data_max_size - i - 1].path,
-                 'label': label},
-                ignore_index=True)
-            df_buf = df_buf._append(
-                {'file_name_1': dir_class_2[i].path, 'file_name_2': dir_class_1[data_max_size - i - 1].path,
-                 'label': label},
-                ignore_index=True)
-    return df_buf
-
-
-def make_siam_dataset(work_folder, max_size_class):
+def cat_class(dir_category_1, dir_category_2):
     data_frame = pd.DataFrame(columns=['file_name_1', 'file_name_2', 'label'])
+    max_size_category = len(dir_category_1)
 
-    for i in range(len(work_folder)):
-        class_1 = make_data_class(work_folder[i], max_size_class)
-        for j in range(len(work_folder)):
-            # class_2 = None
-            if i == j:
-                class_2 = make_data_class(work_folder[j], int(max_size_class))
-            else:
-                class_2 = make_data_class(work_folder[j], int(max_size_class / (len(work_folder) - 1)))
+    label = 0
+    if dir_category_1.iloc[0]['label'] != dir_category_2.iloc[0]['label']:
+        label = 1
 
-            data_frame = data_frame._append(cat_class(class_1, class_2))
+    for i in range(int(max_size_category/2)):
+        data_frame = data_frame._append(
+            {'file_name_1': dir_category_1.iloc[i]['file_name'], 'file_name_2': dir_category_2.iloc[max_size_category - i - 1]['file_name'],
+            'label': label},
+            ignore_index=True)
+        data_frame = data_frame._append(
+            {'file_name_1': dir_category_2.iloc[i]['file_name'], 'file_name_2': dir_category_1.iloc[max_size_category - i - 1]['file_name'],
+            'label': label},
+            ignore_index=True)
+
     return data_frame
 
 
-# 9 category
+def make_dataset(work_folder, max_size_class):
+    data_frame = pd.DataFrame(columns=['file_name', 'label'])
+    for i in range(len(work_folder)):
+        class_buf = make_data_category(work_folder[i])
+        class_buf = class_buf[0:max_size_class]
+
+        for j in range(len(class_buf)):
+            data_frame = data_frame._append({'file_name': class_buf[j].path, 'label': i}, ignore_index=True)
+
+    return data_frame
+
+
+def make_seam_dataset(data_set):
+    data_frame = pd.DataFrame(columns=['file_name_1', 'file_name_2', 'label'])
+
+    category_size = len(data_set[data_set.columns[1]].unique())
+
+    for i in range(category_size):
+        for j in range(category_size):
+            class_1 = data_set.loc[data_set['label'] == i]
+            class_2 = data_set.loc[data_set['label'] == j]
+
+            if i != j:
+                class_1 = class_1[:int(len(class_1)/(category_size-1))]
+                class_2 = class_2[:int(len(class_2)/(category_size-1))]
+
+            data_frame = data_frame._append(cat_class(class_1, class_2))
+
+    data_frame = data_frame.sample(frac=1).reset_index(drop=True)
+    return data_frame
+
+
+# 10 category
 dir_class = list()
 dir_class.append('../../../source/dataRGB/Основное зерно отобранное')
 dir_class.append('../../../source/dataRGB/Пшеница альтернариоз отобранное 2')
@@ -107,21 +113,39 @@ max_size = max_size(dir_class)
 
 print("Max size class: " + str(max_size))
 
-df = make_siam_dataset(dir_class, max_size)
+df = make_dataset(dir_class, max_size)
 
-df["file_name_1"] = df["file_name_1"].str.replace('\\', '/')
+df["file_name"] = df["file_name"].str.replace('\\', '/')
 
-df = df.sample(frac=1).reset_index(drop=True)
+df_train = pd.DataFrame(columns=['file_name', 'label'])
+df_val = pd.DataFrame(columns=['file_name', 'label'])
+df_test = pd.DataFrame(columns=['file_name', 'label'])
 
-df_train = df[:int(len(df)*0.8)]
-df_val = df[int(len(df)*0.8):int(len(df)*0.9)]
-df_test = df[int(len(df)*0.9):]
+for i in range(len(dir_class)):
+    df_buf = df.loc[df['label'] == i]
+    df_train = df_train._append(df_buf[:int(len(df_buf) * 0.8)])
+    df_val = df_val._append(df_buf[int(len(df_buf) * 0.8):int(len(df_buf) * 0.9)])
+    df_test = df_test._append(df_buf[int(len(df_buf) * 0.9):])
 
-df_train.to_csv('siam_data_train.csv', header=None, index=False, encoding="ansi")
-df_val.to_csv('siam_data_val.csv', header=None, index=False, encoding="ansi")
-df_test.to_csv('siam_data_test.csv', header=None, index=False, encoding="ansi")
+df_train = df_train.sample(frac=1).reset_index(drop=True)
+df_val = df_val.sample(frac=1).reset_index(drop=True)
+df_test = df_test.sample(frac=1).reset_index(drop=True)
+
+df_train.to_csv('data_train.csv', header=None, index=False, encoding="ansi")
+df_val.to_csv('data_val.csv', header=None, index=False, encoding="ansi")
+df_test.to_csv('data_test.csv', header=None, index=False, encoding="ansi")
+
 
 print(df['label'].value_counts())
 print(df_train['label'].value_counts())
 print(df_val['label'].value_counts())
 print(df_test['label'].value_counts())
+
+
+seam_train_data_set = make_seam_dataset(df_train)
+seam_val_data_set = make_seam_dataset(df_val)
+seam_test_data_set = make_seam_dataset(df_test)
+
+print(seam_train_data_set['label'].value_counts())
+print(seam_val_data_set['label'].value_counts())
+print(seam_test_data_set['label'].value_counts())
